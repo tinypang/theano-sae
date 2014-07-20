538a57fdb6ccf09bade735189c93112189e6f3a4
tiny <tiny@tinypang.me>
2014-07-21 02:10:42 +1000

    finalised allnecessary files. working on bug fix

--- aeclass.py
+++ aeclass.py
@@ -1,59 +1,20 @@
 import numpy as np
-import theano as T
-from theano import shared as shared
+import theano
+import theano.tensor as T
+from theano import shared
 from theano import config
 
 class Autoencoder(object):
-    def __init__(self, np_rng, input=None, n_vis=784, n_hid=500,W=None, hbias = None, vbias=None):
-		"""
-
-    :type np_rng: numpy.random.RandomState
-    :param np_rng: number random generator used to generate weights
-
-
-    :type input: theano.tensor.TensorType
-    :paran input: a symbolic description of the input or None for standalone
-                  dA
-
-    :type n_vis: int
-    :param n_vis: number of visible units
-
-    :type n_hidden: int
-    :param n_hid:  number of hidden units
-
-    :type W: theano.tensor.TensorType
-    :param W: Theano variable pointing to a set of weights that should be
-              shared belong the dA and another architecture; if dA should
-              be standalone set this to None
-
-    :type hbias: theano.tensor.TensorType
-    :param hbias: Theano variable pointing to a set of biases values (for
-                 hidden units) that should be shared belong dA and another
-                 architecture; if dA should be standalone set this to None
-
-    :type vbias: theano.tensor.TensorType
-    :param vbias: Theano variable pointing to a set of biases values (for
-                 visible units) that should be shared belong dA and another
-                 architecture; if dA should be standalone set this to None
-
-
-    """
-        self.n_vis = n_vis
-        self.n_hid = n_hid
-        # W is initialized with `initial_W` which is uniformely sampled
-        # from -4*sqrt(6./(n_visible+n_hidden)) and 4*sqrt(6./(n_hidden+n_visible))
-        # the output of uniform if converted using asarray to dtype
+    def __init__(self, np_rng, input=None, n_vis=784, n_hid=500, W=None, hbias = None, vbias=None):
         # theano.config.floatX so that the code is runable on GPU
+        
         if not W:
-            init_W = np.asarray(np_rng.uniform(
-                low=-4 * numpy.sqrt(6. / (n_hid + n_vis)),
-                high=4 * numpy.sqrt(6. / (n_hid + n_vis)),
-                size=(n_vis, n_hid)), dtype=theano.config.floatX)
+            init_W = np.asarray(np_rng.uniform(low=-4 * np.sqrt(6. / (n_hid + n_vis)),high=4 * np.sqrt(6. / (n_hid + n_vis)),size=(n_vis, n_hid)), dtype=config.floatX)
             W = shared(value=init_W, name = 'W')
         if not vbias:
-            vbias = shared(value=np.zeroes(n_vis, dtype=theano.config.floatx), name='vbias')
+            vbias = shared(value=np.zeros(n_vis, dtype=config.floatX), name='vbias')
         if not hbias:
-            hbias = shared(value=np.zeroes(n_hid, dtype=theano.config.floatx), name='hbias')
+            hbias = shared(value=np.zeros(n_hid, dtype=config.floatX), name='hbias')
         self.W = W
         self.b_hid = hbias
         self.b_vis = vbias
@@ -66,16 +27,16 @@ class Autoencoder(object):
         self.params = [self.W, self.b_hid, self.b_vis]
 
     def get_hidden_values(self, input):
-    """ computes values of hidden layer """
+        """ computes values of hidden layer """
         return T.nnet.sigmoid(T.dot(input, self.W) + self.b_hid)
 
     def get_reconstructed_input(self, hidden):
-    """ computes the reconstructed input given the values of the hidden layer """
+        """ computes the reconstructed input given the values of the hidden layer """
         return T.nnet.sigmoid(T.dot(hidden, self.W_prime) + self.b_vis)
 
     def get_cost_updates(self, learning_rate):
-    """ computes the cost and the updates for one trainng step """
-        y = self.get_hidden_values(self.x):
+        """ computes the cost and the updates for one trainng step """
+        y = self.get_hidden_values(self.x)
         z = self.get_reconstructed_input(y)
         L = -T.sum(self.x * T.log(z) + (1 - self.x) * T.log(1 - z), axis=1)
         # note : we sum over the size of a datapoint; if we are using minibatches, L will  be a vector, with one entry per example in minibatch.
@@ -84,5 +45,5 @@ class Autoencoder(object):
         gparams = T.grad(cost, self.params) # compute the gradients of the cost of the `dA` with respect to its parameters
         updates = [] #generate list of updates
         for param, gparam in zip(self.params, gparams):
-        updates.append((param, param - learning_rate * gparam))
+            updates.append((param, param - learning_rate * gparam))
         return (cost, updates)
--- aetrain.py
+++ aetrain.py
@@ -1,7 +1,118 @@
-import theano as T
+import cPickle
+import gzip
+import os
+import sys
+import theano
+import theano.tensor as T
 import numpy as np
 import aeclass as ae
+import time
+from theano.tensor.shared_randomstreams import RandomStreams
+from theano import function
+from logistic_sgd import load_data
+from utils import tile_raster_images
 
-autoencoder = Autoencoder(numpy_rng=numpy.random.RandomState(1234), n_vis=784, n_hid=500)
-cost, updates = autoencoder.get_cost_updates(learning_rate=0.1)
-train = theano.function([x], cost, updates=updates)
+import PIL.Image
+'''
+# Load the dataset
+f = gzip.open('mnist.pkl.gz', 'rb')
+train_set, valid_set, test_set = cPickle.load(f)
+f.close()
+
+def shared_dataset(data_xy):
+    """ Function that loads the dataset into shared variables
+
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(numpy.asarray(data_x, dtype=theano.config.floatX))
+    shared_y = theano.shared(numpy.asarray(data_y, dtype=theano.config.floatX))
+    # When storing data on the GPU it has to be stored as floats
+    # therefore we will store the labels as ``floatX`` as well
+    # (``shared_y`` does exactly that). But during our computations
+    # we need them as ints (we use labels as index, and if they are
+    # floats it doesn't make sense) therefore instead of returning
+    # ``shared_y`` we will have to cast it to int. This little hack
+    # lets us get around this issue
+    return shared_x, T.cast(shared_y, 'int32')
+
+test_set_x, test_set_y = shared_dataset(test_set)
+valid_set_x, valid_set_y = shared_dataset(valid_set)
+train_set_x, train_set_y = shared_dataset(train_set)
+
+batch_size = 500    # size of the minibatch
+
+# accessing the third minibatch of the training set
+
+data  = train_set_x[2 * 500: 3 * 500]
+label = train_set_y[2 * 500: 3 * 500]
+'''
+
+def test_dA(learning_rate=0.1, training_epochs=15,
+            dataset='mnist.pkl.gz',
+            batch_size=20, output_folder='dA_plots'):
+
+    """
+    This demo is tested on MNIST
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used for training the DeNosing
+                          AutoEncoder
+
+    :type training_epochs: int
+    :param training_epochs: number of epochs used for training
+
+    :type dataset: string
+    :param dataset: path to the picked dataset
+
+    """
+    #get dataset
+    datasets = load_data(dataset)
+    train_set_x, train_set_y = datasets[0]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()    # index to a [mini]batch
+    x = T.matrix('x')  # the data is presented as rasterized images
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    os.chdir(output_folder)
+
+    #build model
+    rng = np.random.RandomState(123)
+    #theano_rng = RandomStreams(rng.randint(2 ** 30))
+    autoencoder = ae.Autoencoder(np_rng=rng, n_vis=784, n_hid=500)
+    cost, updates = autoencoder.get_cost_updates(learning_rate=0.1)
+    train_ae = function([index], outputs=cost, updates=updates, givens={x: train_set_x[index * batch_size:(index + 1) * batch_size]}, on_unused_input = 'warn', name='train_ae')
+    #train = theano.function([x], cost, updates=updates, on_unused_input='warn') 
+    start_time = time.clock ()
+
+    #training
+
+    for epoch in xrange(training_epochs):
+        # go through training set
+        c = []
+        for batch_index in xrange(n_train_batches):
+            c.append(train_ae(batch_index))
+
+        print 'Training epoch %d, cost ' % epoch, np.mean(c)
+
+    end_time = time.clock
+
+    training_time = (end_time - start_time)
+
+    print ('Training took %f minutes' % (pretraining_time / 60.))
+    image = PIL.Image.fromarray(
+        tile_raster_images(X=da.W.get_value(borrow=True).T,
+                           img_shape=(28, 28), tile_shape=(10, 10),
+                           tile_spacing=(1, 1)))
+    image.save('filters_corruption_0.png')
+
+if __name__ == '__main__':
+    test_dA()
