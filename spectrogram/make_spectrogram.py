cce6866e1d80f551675c7d7ae1aa94f490f20fe5
tiny <tiny@tinypang.me>
2014-08-03 01:05:24 +1000

    added pca code

--- spectrogram/make_spectrogram.py
+++ spectrogram/make_spectrogram.py
@@ -13,18 +13,24 @@ import pylab
 import matplotlib
 import matplotlib.pyplot as plt
 
+num_frames={}
+
 def graph_spectrogram(au_file,name):
-    sound_info, frame_rate = get_wav_info(au_file)
+    sound_info, frame_rate, nframes = get_wav_info(au_file)
     fig=plt.figure(num=None, frameon=False)
     fig.set_size_inches(19,12)
     #plt.subplot(111)
     #plt.title('spectrogram of %r' % au_file)
-    ax = plt.Axes(fig, [0., 0., 1.09,1.09])
+    if nframes == 661504:
+        ax = plt.Axes(fig, [0., 0., 1.,1.09])
+    else:
+        ax = plt.Axes(fig, [0., 0., 1.18, 1.09])
     ax.set_axis_off()
     fig.add_axes(ax)
     spec = plt.specgram(sound_info, Fs=frame_rate)
-    print name
-    fig.savefig('Spectrograms/1raw-spectrogram-%s.png' % os.path.splitext(name)[0],format='png')
+    print name, nframes
+    print spec
+    #ig.savefig('Spectrograms/{0}-raw-spectrogram-{1}.png'.format(os.path.splitext(name)[0],nframes),format='png')
     plt.close(fig)
 
 def get_wav_info(au_file):
@@ -32,26 +38,38 @@ def get_wav_info(au_file):
     frames = au.readframes(-1)
     sound_info = pylab.fromstring(frames, 'Int16')
     frame_rate = au.getframerate()
-    print au.getnframes()
+    nframes = au.getnframes()
     au.close()
-    return sound_info, frame_rate
+    return sound_info, frame_rate, nframes
 
 def explore(path):
     sounds = glob.glob(path + '/*.au')
-    num_frames = {}
     for filename in os.listdir(path):
         if sounds.count(path + '/' + filename) != 0:
+            '''
+            au_file = path + '/' + filename
             au = sunau.open(au_file, 'r')
             frames = au.getnframes()
             if frames in num_frames:
-                num_frames[frames] = num_frames[frames+1]
+                num_frames[frames] = num_frames[frames]+1
             else:
-                num_frames[franes] = 1
-            #graph_spectrogram(path + '/' + filename, filename)
+                num_frames[frames] = 1 
+            '''
+            graph_spectrogram(path + '/' + filename, filename)
         else:
             explore(path + '/' + filename)
-    print num_frames
+'''
+    def main():
+    explore('../gtzan_genre')
+    keylist = num_frames.keys()
+    keylist.sort()
+    for i in keylist:
+        print i, num_frames[i]
+    return
+'''
 
 if __name__ == '__main__':
-    explore('../gtzan_genre')
-    
+    #main()
+    explore('../pca/test')
+    #explore('../gtzan_genre')
+
