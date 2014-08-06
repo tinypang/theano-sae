59717130c288cad16e3646e81aa6a5293e56a6d4
tiny <tiny@tinypang.me>
2014-08-06 20:44:39 +1000

    added audio snippet file

---
+++ pca_whiten.py
@@ -0,0 +1,67 @@
+from scipy import misc
+import os
+import numpy as np
+import Image
+import matplotlib.pyplot as plt
+from sklearn.decomposition import RandomizedPCA
+import time
+
+standard_size = (1900,1200) #standard size for all spectrograms
+
+def normalize(arr): #linearly nromalize each pixel in an image
+    # Do not touch the alpha channel
+    for i in range(3):  #for rgb values
+        minval = arr[...,i].min()
+        maxval = arr[...,i].max()
+        if minval != maxval:
+            np.subtract(arr[...,i],minval)
+            np.multiply(arr[...,i],(255.0/(maxval-minval)))
+    return arr
+
+def img_to_rgbmat(filename):    #convert image to array of rgba values of each pixel
+    img = Image.open(filename)  #open image
+    img = img.convert('RGB')    #convert image to rgb
+    img = img.resize(standard_size) #resize image to standard size
+    img = list(img.getdata()) #get sequence object with pixel values for image and convert to a normal python list object
+    img = map(list, img) #convert each sequence object row of pixels into a python list row
+    img = np.array(img) #convert 2D list array into a numpy array
+    img = normalize(img)
+    return img
+
+def flatten_img(img): #converts an (m,n) numpy array to an array of shape (1, m*n)
+    s = img.shape[0] * img.shape[1] #calculate number of pixels in the img
+    img_wide = img.reshape(1,s)     #reshape img to new size
+    return img_wide[0]
+
+#def zero_mean(data):   
+
+def pca_whiten(path):
+    data = []
+    n = 0
+    pt0 = time.time()
+    for filename in os.listdir(path):   #convert all images in a given path to rgb matrices, 
+        imgfile = path + '/' + filename #flatten and append to list containing data for all images
+        #pt1 = time.time()
+        img = img_to_rgbmat(imgfile)
+        #pt2 = time.time()
+        img = flatten_img(img)
+        #pt3 = time.time()
+        data.append(img)
+        n+=1
+        print n
+    data = np.array(data)   #convert python list of all img data to a numy array
+    pt4 = time.time()
+    print 'import and normalization took time {0}'.format(pt4 - pt0)
+    pca = RandomizedPCA(n_components=10, whiten=True)
+    X = pca.fit_transform(data)
+    pt5 = time.time()
+    print 'pca whitening took time {0}'.format(pt5 - pt4)
+    print 'total time taken {0}'.format(pt5-pt0)
+    return X
+    #savefile = open('pca/pca_spectrograms_savefile.txt', 'w')
+    #savefile.write(str(X))
+    #savefile.close()
+
+if __name__ == '__main__':
+    #pca_whiten('./test')
+    pca_whiten('../spectrogram/Spectrograms/Sample50')
